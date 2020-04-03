3fa9cfedc2a63cc7ef184edfd93ddc7234dd52a3
OlegBalabukh <olegukoff@gmail.com>
2020-04-03 19:51:40 +0300

    Created GET route for getting logged user's tasks

--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+
+const Task = require('../../models/Task');
 
 // @route     GET api/tasks
-// @desc      Test route
-// @access    Public
-router.get('/', (req, res) => res.send('Task route'));
+// @desc      Get current user tasks
+// @access    Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const tasks = await Task.find().sort({ date: -1 });
+
+    if (!tasks.length) {
+      return res.status(400).json({ msg: 'There are no tasks for this user' });
+    }
+
+    res.json(tasks);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
