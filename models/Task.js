9c71985a2b293bfe652902f19c4e79838d5c7f41
OlegBalabukh <olegukoff@gmail.com>
2020-04-03 18:59:30 +0300

    Created Task model

---
+++ models/Task.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const TaskSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Task = mongoose.model('task', TaskSchema);
