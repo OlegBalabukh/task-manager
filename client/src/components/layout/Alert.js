84934ecbbddafc8b3525e568ec3ca1e707e55e19
OlegBalabukh <olegukoff@gmail.com>
2020-04-10 15:31:42 +0300

    Absolute position for success alerts

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -6,7 +6,12 @@ const Alert = ({ alerts }) =>
   alerts !== null &&
   alerts.length > 0 &&
   alerts.map((alert) => (
-    <div key={alert.id} className={`alert alert-${alert.alertType}`}>
+    <div
+      key={alert.id}
+      className={`alert alert-${alert.alertType} ${
+        alert.alertType === 'success' && 'alert-absolute'
+      }`}
+    >
       {alert.msg}
     </div>
   ));
